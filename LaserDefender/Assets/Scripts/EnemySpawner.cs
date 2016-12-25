4381323b0ef5dc056b04f4cd9694bf09ff4fdd77
mahyar121 <mahyarhajibabaie@gmail.com>
2016-12-25 10:40:13 -0800

    Programmed the Enemy Spawner

--- LaserDefender/Assets/Scripts/EnemySpawner.cs
+++ LaserDefender/Assets/Scripts/EnemySpawner.cs
@@ -10,7 +10,14 @@ public class EnemySpawner : MonoBehaviour
 	// Use this for initialization
 	void Start ()
     {
-        Instantiate(enemyPrefab, new Vector3(0f, 0f, 0f), Quaternion.identity);
+       // for every child in parent spawn an enemy at location
+        foreach(Transform child in transform)
+        {
+            // Spawns an enemy at (0,0,0) and have to declare as GameObject otherwise would be just an object
+            GameObject enemy = Instantiate(enemyPrefab, new Vector3(0f, 0f, 0f), Quaternion.identity) as GameObject;
+            // Puts the spawned enemy inside the GameObject as a child
+            enemy.transform.parent = childd;
+        }
 	}
 	
 	// Update is called once per frame
---
+++ LaserDefender/Assets/Scripts/Position.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Position : MonoBehaviour
+{
+    // Gizmos is the outline of the objects, The camera has the square gizmo
+    private void OnDrawGizmos()
+    {
+        // (Location, Radius)
+        Gizmos.DrawWireSphere(transform.position, 1);
+    }
+    
+}
